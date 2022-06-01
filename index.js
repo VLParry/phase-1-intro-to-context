125cb24a02c5453c1ee82f415f581c4283660bad
Victoria Parry <vlparry@gmail.com>
2022-06-01 14:09:05 -0400

    commit tree 430820ed7bf921083a33bddfc2990c8f9857be0e

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Your code here
+function createEmployeeRecord(employee) {
+    return {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(employees) {
+    return employees.map(employee => createEmployeeRecord(employee))
+}
+
+function createTimeInEvent(event) {
+    let [date, hour] = event.split(" ")
+    let eventObj = {
+        type: "TimeIn",
+        hour: "event",
+        date: "event"
+    }
+    employee.timeInEvents = [...employee.timeInEvents, eventObj]
+
+}
